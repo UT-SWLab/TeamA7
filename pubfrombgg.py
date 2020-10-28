4ac6ddd002dac8712a80217e723b9f4b27e979bd
cedrik <cedrikho@gmail.com>
2020-10-28 00:35:32 -0500

    Main is imported to tie DB publisher list to necessary comparison.  for other API BBG.

--- pubfrombgg.py
+++ pubfrombgg.py
@@ -1,6 +1,26 @@
 #If you're unsure what's happening here feel free to ask me (Sanne)
 import requests
 import xml.etree.ElementTree as ET
+import main
+
+
+
+def PublisherNames():
+    #Function now returns tupple of list to so game and publisher are tied for publisher page elements.
+    bgc = main.boardgameobjects.find()
+    publishernames = []
+
+    for game in bgc:
+        if game['Publisher'] not in publishernames:
+            publishernames.append(game['Publisher'])
+    print (publishernames) #Debuggin
+    return publishernames
+
+
+ListofPublishers = PublisherNames() # Get all publishers we are using, this is from DB and filtered and built in main.oy
+
+
+
 #Board Game Geek uses xml instead of json, so the interpreting of data is different
 #This goes up pretty high, 810 at least
 resp = requests.get("https://www.boardgamegeek.com/xmlapi/boardgamepublisher/3", stream=True)
@@ -11,3 +31,5 @@ CompanyName = responseXml.find('company').find('name')
 print(CompanyName.text)
 CompanyDescription = responseXml.find('company').find('description')
 print(CompanyDescription.text)
+
+
