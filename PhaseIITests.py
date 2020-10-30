682e206de0afdccd36c6bb77281712c89c83721f
unknown <sbloemsma@utexas.edu>
2020-10-29 19:40:40 -0500

    more list page tests

--- PhaseIITests.py
+++ PhaseIITests.py
@@ -1,7 +1,6 @@
 import unittest
 from flask import abort, url_for
-from flask_testing import TestCase
-
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -49,21 +48,66 @@ class TestsTeamA7(unittest.TestCase):
         self.assertEqual(self.driver.title, 'Board Games Publisher List')
         header_with_page_number = self.driver.find_element(By.TAG_NAME,"h1")
         self.assertEqual(header_with_page_number.text, "Publishers Page 1")
-        publisherbutton = self.driver.find_element_by_id("publishersnav")
-        publisherbutton.click()  # should take you to Publishers List Page
+        genresbutton = self.driver.find_element_by_id("genresnav")
+        genresbutton.click()  # should take you to Genres List Page
         self.driver.implicitly_wait(5)
-        self.assertEqual(self.driver.title, 'Board Games Genre List')
+        self.assertEqual(self.driver.title, 'Board Game Genres List')
         header_with_page_number = self.driver.find_element(By.TAG_NAME,"h1")
-        self.assertEqual(header_with_page_number.text, "Publishers Page 1")
-    #Tests that
-    def test_list_load_time(self):
+        self.assertEqual(header_with_page_number.text, "Genres Page 1")
+
+    #Starting from a random game from board game lists page
+    def test_model_linking_board_game_list(self):
         #testing board games list generation
         gamesbutton = self.driver.find_element_by_id("boardgamesnav")
         gamesbutton.click()  # should take you to Board games Page
-        self.driver.implicitly_wait(5)
         self.assertEqual(self.driver.title, 'Board Games List')
-        header_with_page_number = self.driver.find_element(By.TAG_NAME,"h1")
-        self.assertEqual(header_with_page_number.text, "Board Games Page 1")
+        games_buttons_on_page = self.driver.find_elements(By.ID,"gamename")
+        random_game = games_buttons_on_page[random.randint(0,len(games_buttons_on_page))]
+        gamename = random_game.text
+        random_game.click()
+        self.assertEqual(self.driver.find_element(By.TAG_NAME,'h2').text, gamename)
+        gamesbutton = self.driver.find_element_by_id("boardgamesnav")
+        gamesbutton.click()  # should take you to Board games Page
+        self.assertEqual(self.driver.title, 'Board Games List')
+        publisher_buttons_on_page = self.driver.find_elements(By.ID,"publishername")
+        random_publisher = publisher_buttons_on_page[random.randint(0,len(publisher_buttons_on_page))]
+        publishername = random_publisher.text
+        random_publisher.click()
+        self.assertEqual("Publisher: " + self.driver.find_element(By.TAG_NAME,'h2').text, publishername)
+        gamesbutton = self.driver.find_element_by_id("boardgamesnav")
+        gamesbutton.click()  # should take you to Board games Page
+        self.assertEqual(self.driver.title, 'Board Games List')
+
+    def test_model_board_game_list(self):
+        #testing board games list links
+        gamesbutton = self.driver.find_element_by_id("boardgamesnav")
+        gamesbutton.click()  # should take you to Board games Page
+        self.assertEqual(self.driver.title, 'Board Games List')
+        games_buttons_on_page = self.driver.find_elements(By.ID,"gamename") #all games visible on this page
+        total_for_page = len(games_buttons_on_page)
+        for i in range(0,total_for_page):
+            name = games_buttons_on_page[i].text
+            games_buttons_on_page[i].click()
+            self.assertEqual(self.driver.find_element(By.TAG_NAME, 'h2').text, name)
+            self.driver.find_element_by_id("boardgamesnav").click()
+            games_buttons_on_page = self.driver.find_elements(By.ID,"gamename")
+
+    def test_model_genres_list(self):
+        #testing board game genres list generation
+        genresbutton = self.driver.find_element_by_id("genresnav")
+        genresbutton.click()  # should take you to Board games Page
+        self.assertEqual(self.driver.title, 'Board Game Genres List')
+        genres_buttons_on_page = self.driver.find_elements(By.ID,"genrename") #all games visible on this page
+        total_for_page = len(genres_buttons_on_page)
+        for i in range(0,total_for_page):
+            name = genres_buttons_on_page[i].text
+            genres_buttons_on_page[i].click()
+            self.assertEqual(self.driver.find_element(By.TAG_NAME, 'h2').text, name)
+            self.driver.find_element_by_id("genresnav").click()
+            genres_buttons_on_page = self.driver.find_elements(By.ID,"genrename")
+
+
+
     def tearDown(self):
         self.driver.quit()
 
