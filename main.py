9a7bdf9c4574fe2a92d1631493393bc92882464f
unknown <sbloemsma@utexas.edu>
2020-11-06 23:14:48 -0600

    search route and page added, no search in base yet

--- main.py
+++ main.py
@@ -22,6 +22,31 @@ def GameLinkify(name):
     linkformat = re.compile('[^a-zA-Z0-9]')
     linkname = linkformat.sub('', name)
     return linkname
+# input = string searched by user
+# models = board games, genres, publishers (can be any combo of 3)
+# gamefields = if looking for board games what field are we searching in (default all)
+# Name, Publisher, Genre, Min_Players, etc
+# genrefields = if looing for genres what fields are we searching in (default all)
+# publisherfields = if looking for publishers what fields are we searching in (default all)
+def search(input, models, gamefields, genrefields, publisherfields):
+    exactmatches = {} # not case sensitive, array of all exact matches
+    partialmatches = {} # not case sensitive, dict of all partial matches with key the word they're matched to
+    if models['boardgames']:
+        exactmatches['boardgames'] = list(boardgameobjects.find({"Name": { "$regex": "^"+input+"$", '$options': 'i'}}))
+    if models['genres']:
+        exactmatches['genres'] = list(genre_objects.find({"Name": { "$regex": "^"+input+"$", '$options': 'i'}}))
+    if models['publishers']:
+        exactmatches['publishers'] = list(publish_objects.find({"Name": { "$regex": "^"+input+"$", '$options': 'i'}}))
+    partialwords = input.split()
+    for word in partialwords:
+        partialmatches[word] = {}
+        if models['boardgames']:
+            partialmatches[word]['boargames'] = (list(boardgameobjects.find({"Name": {"$regex": "^" + word + "$", '$options': 'i'}})))
+        if models['genres']:
+            partialmatches[word]['genres'] = (list(genre_objects.find({"Name": {"$regex": "^" + word + "$", '$options': 'i'}})))
+        if models['publishers']:
+            partialmatches[word]['publishers'] = list(publish_objects.find({"Name": {"$regex": "^" + word + "$", '$options': 'i'}}))
+    return exactmatches,partialmatches
 def PublisherNames():
     #Function now returns tupple of list to so game and publisher are tied for publisher page elements.
     bgc = boardgameobjects.find()
@@ -134,6 +159,11 @@ def GamePage(gamelink):
     global gamepagerequest
     game = boardgameobjects.find({'Name': gamepagerequest}).next()
     return render_template("Board_Game_Template.html", game=game)
-
+############ ROUTE TO SEARCH RESULTS ############
+@app.route('/search', methods=['POST'])
+def search():
+    input_string = request.form['search']
+    exactmatches, partialmatches = search(input_string,{'boardgames':True,'genres':True,'publishers':True}, 0, 0 ,0)
+    return render_template("searchresults.html", input_string=input_string, exactmatches=exactmatches, partialmatches=partialmatches)
 if __name__ == "__main__":
     app.run(debug=True)
