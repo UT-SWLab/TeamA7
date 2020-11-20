3f2099d8a6ca487af0520283638f7dc22a1aadf8
Grant-Ross <ghross@utexas.edu>
2020-11-20 15:58:04 -0600

    genre list fix

--- test_search.py
+++ test_search.py
@@ -1,7 +1,7 @@
 
 import unittest
 from main import app
-from main import search
+from main import searchdb
 
 
 class SearchTest(unittest.TestCase):
@@ -9,9 +9,31 @@ class SearchTest(unittest.TestCase):
         self.app = app.test_client()
 
     def test_search(self):
-        search = self.app.get('/search')
+        search, search2 = searchdb('Scrabble', {'boardgames': True, 'genres': True, 'publishers': True}, ['Name'])
+        print(search)
+        self.assertTrue(len(search['boardgames']) == 1)
+        self.assertTrue(len(search['publishers']) == 0)
+        self.assertTrue(len(search['genres']) == 0)
 
-        self.assertTrue('TeamA7 Project Home' in str(home.data))
+        search, search2 = searchdb('Scrabble', {'boardgames': True, 'genres': True, 'publishers': True}, ['Name'])
+        print(search)
+        self.assertTrue(len(search['boardgames']) == 1)
+        self.assertTrue(len(search['publishers']) == 0)
+        self.assertTrue(len(search['genres']) == 0)
+
+        search, search2 = searchdb('Scrabble', {'boardgames': True, 'genres': True, 'publishers': True}, ['Name'])
+        print(search)
+        self.assertTrue(len(search['boardgames']) == 1)
+        self.assertTrue(len(search['publishers']) == 0)
+        self.assertTrue(len(search['genres']) == 0)
+
+        search, search2 = searchdb('Scrabble', {'boardgames': True, 'genres': True, 'publishers': True}, ['Name'])
+        print(search)
+        self.assertTrue(len(search['boardgames']) == 1)
+        self.assertTrue(len(search['publishers']) == 0)
+        self.assertTrue(len(search['genres']) == 0)
+
+        # self.assertTrue('TeamA7 Project Home' in str(home.data))
 
     def tearDown(self):
         pass
