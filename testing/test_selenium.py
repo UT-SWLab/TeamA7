327967ad2266fad44968b91c65ee6dde18cc6e0b
Allegra Thomas <at.allegrathomas@gmail.com>
2020-11-29 23:38:02 -0600

    refactored testing

--- testing/test_selenium.py
+++ testing/test_selenium.py
@@ -3,11 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #so we can access main from parent directory
+from main import noFilter
+from main import SelectFilter
+from main import ApplyFoundFilters
+import pymongo
+from pymongo import MongoClient
+from mongoengine import *
+import time
 
-class TestsTeamA7(unittest.TestCase):
+
+##################################################SITE FRONT END TESTING#########################################################
+
+class FrontEndTests(unittest.TestCase):
     #Test Home Carousel takes you to legitimate pages
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path= r"C:\Users\sbloe\Desktop\geckodriver.exe")
+        self.driver = webdriver.Firefox(executable_path=r"/Users/Allegra/Desktop/EE 461L/Tutorial 8/part2/geckodriver")
+        # self.driver = webdriver.Firefox(executable_path= r"C:\Users\sbloe\Desktop\geckodriver.exe")
         #self.driver.get(r"https://fall-2020-ee461l-teama7.uc.r.appspot.com/")
         self.driver.get(r"http://127.0.0.1:5000/")
 
@@ -31,13 +44,16 @@ class TestsTeamA7(unittest.TestCase):
         self.assertEqual(self.driver.title, 'Board Game Publishers List')
 
     def test_model_board_game_list(self):
+        print("board game list page test")
         #testing board games list pagination and links for default
         self.driver.find_element_by_id("boardgamesnav").click()  # should take you to Board games Page
         self.driver.implicitly_wait(3)
         self.assertEqual(self.driver.title, 'Board Game List')
         #get all pagination buttons, we shall skip first and last as those are previous and next
         pagination_count = len(self.driver.find_elements(By.CLASS_NAME, "page-link"))
-        for page in range(0,pagination_count-2):
+        print(pagination_count)
+        for page in range(0, pagination_count - 1):
+            print(page)
             self.driver.find_elements(By.CLASS_NAME, "page-link")[page].click()
             total_for_page = len(self.driver.find_elements(By.ID,"gamename"))
             for i in range(0, total_for_page):
@@ -48,13 +64,16 @@ class TestsTeamA7(unittest.TestCase):
                 self.driver.find_elements(By.CLASS_NAME, "page-link")[page].click()
 
     def test_model_genres_list(self):
+        print("genre list page tests")
         # testing board games genres list pagination and links for default
         genrebutton = self.driver.find_element_by_id("genresnav")
         genrebutton.click()  # should take you to Board Genres Page
         self.assertEqual(self.driver.title, 'Board Game Genres List')
         # get all pagination buttons, we shall skip first and last as those are previous and next
         pagination_count = len(self.driver.find_elements(By.CLASS_NAME, "page-link"))
-        for page in range(0, pagination_count - 2):
+        print(pagination_count)
+        for page in range(0, pagination_count - 1):
+            print(page)
             self.driver.find_elements(By.CLASS_NAME, "page-link")[page].click()
             total_for_page = len(self.driver.find_elements(By.ID, "genrename"))
             for i in range(0, total_for_page):
@@ -65,12 +84,15 @@ class TestsTeamA7(unittest.TestCase):
                 self.driver.find_elements(By.CLASS_NAME, "page-link")[page].click()
 
     def test_model_publishers_list(self):
+        print("publisher list page test")
         # testing board games publishers list pagination and links for default
         self.driver.find_element_by_id("publishersnav").click()
         self.assertEqual(self.driver.title, 'Board Game Publishers List')
         # get all pagination buttons, we shall skip first and last as those are previous and next
         pagination_count = len(self.driver.find_elements(By.CLASS_NAME, "page-link"))
-        for page in range(0, pagination_count - 2):
+        print(pagination_count)
+        for page in range(0, pagination_count - 1):
+            print(page)
             self.driver.find_elements(By.CLASS_NAME, "page-link")[page].click()
             total_for_page = len(self.driver.find_elements(By.ID, "publishername"))
             for i in range(0, total_for_page):
@@ -84,6 +106,7 @@ class TestsTeamA7(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
